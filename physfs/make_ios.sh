e881f114533a40c1d925ae57335173fd2efa8d35
whiteye <18351967270@163.com>
2023-06-17 18:57:09 +0800

    Update make_ios.sh

--- physfs/make_ios.sh
+++ physfs/make_ios.sh
@@ -1,4 +1,4 @@
 mkdir -p build_ios && cd build_ios
 cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DPLATFORM=OS64 -DPHYSFS_DISABLE_INSTALL=ON -GXcode ../
 cd ..
-cmake --build build_ios --config Release
+cmake --build build_ios --config Release --CODE_SIGNING_ALLOWED=NO
